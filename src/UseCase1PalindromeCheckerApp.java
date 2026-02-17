4b1440d3681487f7e87461d16a621049ab5170b1
Vidush1224 <vidush.skv@gmail.com>
2026-02-17 09:41:51 +0530

    UC1: Implement application entry point and welcome message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String appName = "Palindrome Checker Management System";
+        String version = "Version 1.0";
+
+        System.out.println("Welcome to " + appName);
+        System.out.println(version);
+
+        System.out.println("System initialized successfully.");
+    }
+}
